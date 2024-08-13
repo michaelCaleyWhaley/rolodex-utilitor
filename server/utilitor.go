c00d35090731cfd289880db1981fe26defe6d15a
Michael Caley <kneedeepwater@hotmail.com>
2024-08-13 18:44:14 +0100

    test gin proxy response

--- server/utilitor.go
+++ server/utilitor.go
@@ -1,79 +1,89 @@
-// package main
-
-// import (
-// 	"context"
-// 	"log"
-// 	"os"
-
-// 	"github.com/aws/aws-lambda-go/events"
-// 	"github.com/aws/aws-lambda-go/lambda"
-// 	ginadapter "github.com/awslabs/aws-lambda-go-api-proxy/gin"
-// 	"github.com/gin-gonic/gin"
-// )
-
-// var ginLambda *ginadapter.GinLambda
-
-// func routes(r *gin.Engine) {
-// 	r.Use(func(c *gin.Context) {
-// 		c.Header("Access-Control-Allow-Origin", "*")
-// 	})
-
-// 	r.GET("/ping", func(c *gin.Context) {
-// 		c.JSON(200, gin.H{
-// 			"message": "pong",
-// 		})
-// 	})
-
-// }
-
-// func init() {
-// 	// stdout and stderr are sent to AWS CloudWatch Logs
-// 	log.Printf("Gin cold start")
-// 	r := gin.Default()
-// 	routes(r)
-
-// 	isLocal := os.Args[len(os.Args)-1] == "--local"
-// 	if isLocal {
-// 		r.Run("localhost:4000")
-// 		return
-// 	}
-// 	ginLambda = ginadapter.New(r)
-
-// }
-
-// func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-// 	// If no name is provided in the HTTP request body, throw an error
-// 	return ginLambda.ProxyWithContext(ctx, req)
-// }
-
-// func main() {
-// 	isLocal := os.Args[len(os.Args)-1] == "--local"
-// 	if isLocal {
-// 		return
-// 	}
-// 	lambda.Start(Handler)
-// }
-
 package main
 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
+	"log"
+	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	ginadapter "github.com/awslabs/aws-lambda-go-api-proxy/gin"
+	"github.com/gin-gonic/gin"
 )
 
-func HandleRequest(ctx context.Context, event events.LambdaFunctionURLRequest) (events.APIGatewayProxyResponse, error) {
+var ginLambda *ginadapter.GinLambda
 
-	data := map[string]string{"test": "test string"}
-	responseJSON, err := json.Marshal(data)
-	fmt.Println(err)
+func routes(r *gin.Engine) {
+	r.Use(func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Origin", "*")
+	})
 
-	return events.APIGatewayProxyResponse{Body: string(responseJSON), StatusCode: 200}, nil
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+}
+
+func init() {
+	// stdout and stderr are sent to AWS CloudWatch Logs
+	log.Printf("Gin cold start")
+	r := gin.Default()
+	routes(r)
+
+	isLocal := os.Args[len(os.Args)-1] == "--local"
+	if isLocal {
+		r.Run("localhost:4000")
+		return
+	}
+	ginLambda = ginadapter.New(r)
+
+}
+
+func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	// If no name is provided in the HTTP request body, throw an error
+	return ginLambda.ProxyWithContext(ctx, req)
 }
 
 func main() {
-	lambda.Start(HandleRequest)
+	isLocal := os.Args[len(os.Args)-1] == "--local"
+	if isLocal {
+		return
+	}
+	lambda.Start(Handler)
 }
+
+// #####################################
+// Simple working example
+// #####################################
+
+// package main
+
+// import (
+// 	"context"
+// 	"encoding/json"
+// 	"fmt"
+
+// 	"github.com/aws/aws-lambda-go/events"
+// 	"github.com/aws/aws-lambda-go/lambda"
+// )
+
+// func HandleRequest(ctx context.Context, event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+// 	data := map[string]string{"test": "test string"}
+// 	responseJSON, err := json.Marshal(data)
+// 	fmt.Println(err)
+
+// 	return events.APIGatewayProxyResponse{Body: string(responseJSON), StatusCode: 200}, nil
+// }
+
+// func main() {
+// 	lambda.Start(HandleRequest)
+// }

6add1b0f6b67ecb9dc47e99932304e76b013a6b0
Michael Caley <kneedeepwater@hotmail.com>
2024-08-13 19:18:54 +0100

    Dynamic api endpoint
    * create logs
    
    * streamline response
    
    * test map instead of JSON
    
    * test no marshalling
    
    * shift env example
    
    * return simple struct
    
    * return lambda event
    
    * test stripped lambda
    
    * test simple string
    
    * test empty json
    
    * test with header
    
    * test with cookie
    
    * respond with json
    
    * return correct go lang response
    
    * test json response
    
    * test gin proxy response
    
    * update todo

--- platform_config/api-gateway.tf
+++ platform_config/api-gateway.tf
@@ -14,16 +14,22 @@ resource "aws_api_gateway_rest_api" "rolodex_utilitor_handler_api_gateway" {
   }
 }
 
+resource "aws_api_gateway_resource" "rolodex_utilitor_handler_api_gateway_resource" {
+  rest_api_id = aws_api_gateway_rest_api.rolodex_utilitor_handler_api_gateway.id
+  parent_id   = aws_api_gateway_rest_api.rolodex_utilitor_handler_api_gateway.root_resource_id
+  path_part   = "{proxy+}"
+}
+
 resource "aws_api_gateway_method" "rolodex_utilitor_handler_api_gateway_method_post" {
   rest_api_id   = aws_api_gateway_rest_api.rolodex_utilitor_handler_api_gateway.id
-  resource_id   = aws_api_gateway_rest_api.rolodex_utilitor_handler_api_gateway.root_resource_id
+  resource_id   = aws_api_gateway_resource.rolodex_utilitor_handler_api_gateway_resource.id
   http_method   = "POST"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "rolodex_utilitor_handler_integration_post" {
   rest_api_id             = aws_api_gateway_rest_api.rolodex_utilitor_handler_api_gateway.id
-  resource_id             = aws_api_gateway_rest_api.rolodex_utilitor_handler_api_gateway.root_resource_id
+  resource_id             = aws_api_gateway_resource.rolodex_utilitor_handler_api_gateway_resource.id
   http_method             = aws_api_gateway_method.rolodex_utilitor_handler_api_gateway_method_post.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
@@ -32,14 +38,14 @@ resource "aws_api_gateway_integration" "rolodex_utilitor_handler_integration_pos
 
 resource "aws_api_gateway_method" "rolodex_utilitor_handler_api_gateway_method" {
   rest_api_id   = aws_api_gateway_rest_api.rolodex_utilitor_handler_api_gateway.id
-  resource_id   = aws_api_gateway_rest_api.rolodex_utilitor_handler_api_gateway.root_resource_id
+  resource_id   = aws_api_gateway_resource.rolodex_utilitor_handler_api_gateway_resource.id
   http_method   = "GET"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "rolodex_utilitor_handler_integration" {
   rest_api_id             = aws_api_gateway_rest_api.rolodex_utilitor_handler_api_gateway.id
-  resource_id             = aws_api_gateway_rest_api.rolodex_utilitor_handler_api_gateway.root_resource_id
+  resource_id             = aws_api_gateway_resource.rolodex_utilitor_handler_api_gateway_resource.id
   http_method             = aws_api_gateway_method.rolodex_utilitor_handler_api_gateway_method.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"

3308f4d785f2163abe25f4616ba6ddc2700b45af
Michael Caley <kneedeepwater@hotmail.com>
2024-08-27 20:14:30 +0100

    add ability to set cookie

--- platform_config/cloudfront.tf
+++ platform_config/cloudfront.tf
@@ -24,7 +24,6 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
 
   origin {
     domain_name = "${aws_api_gateway_rest_api.rolodex_utilitor_handler_api_gateway.id}.execute-api.${local.region}.amazonaws.com"
-    # domain_name = "nh0hmn0bzk.execute-api.eu-west-2.amazonaws.com"
     origin_id   = local.api_origin_id
     origin_path = "/deployed"
 
@@ -49,12 +48,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
 
   viewer_certificate {
     cloudfront_default_certificate = true
-    # acm_certificate_arn      = aws_acm_certificate.root_cert.arn
-    # ssl_support_method       = "sni-only"
-    # minimum_protocol_version = "TLSv1.2_2021"
   }
-
-  # aliases = ["www.caleycodelab.com"]
 
   default_cache_behavior {
     allowed_methods  = ["GET", "HEAD"]
@@ -93,6 +87,15 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     path_pattern           = "/api/*"
     target_origin_id       = local.api_origin_id
     viewer_protocol_policy = "redirect-to-https"
+
+    # forwarded_values {
+    #   query_string = true
+    #   headers      = ["*"]
+
+    #   cookies {
+    #     forward = "all"
+    #   }
+    # }
   }
 
   ordered_cache_behavior {
@@ -125,64 +128,3 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   }
 
 }
-
-# ########## redirect
-# resource "aws_cloudfront_distribution" "s3_distribution_redirect" {
-#   origin {
-#     domain_name = aws_s3_bucket_website_configuration.caley-code-lab-website-config-redirect.website_endpoint
-#     origin_id   = local.s3_origin_id
-
-#     custom_origin_config {
-#       http_port                = 80
-#       https_port               = 443
-#       origin_keepalive_timeout = 5
-#       origin_protocol_policy   = "http-only"
-#       origin_read_timeout      = 30
-#       origin_ssl_protocols = [
-#         "TLSv1.2",
-#       ]
-#     }
-#   }
-
-#   enabled             = true
-#   is_ipv6_enabled     = true
-#   default_root_object = ""
-
-
-#   viewer_certificate {
-#     acm_certificate_arn      = aws_acm_certificate.root_cert.arn
-#     ssl_support_method       = "sni-only"
-#     minimum_protocol_version = "TLSv1.2_2021"
-#   }
-
-#   aliases = ["caleycodelab.com"]
-
-#   default_cache_behavior {
-#     allowed_methods  = ["GET", "HEAD"]
-#     cached_methods   = ["GET", "HEAD"]
-#     target_origin_id = local.s3_origin_id
-
-#     forwarded_values {
-#       query_string = true
-
-#       cookies {
-#         forward = "all"
-#       }
-#     }
-
-#     viewer_protocol_policy = "redirect-to-https"
-#     min_ttl                = 100
-#     default_ttl            = 3600
-#     max_ttl                = 86400
-#   }
-
-#   price_class = "PriceClass_100"
-
-#   restrictions {
-#     geo_restriction {
-#       restriction_type = "blacklist"
-#       locations        = ["IQ"]
-#     }
-#   }
-
-# }
